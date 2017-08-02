5118f8169d21b4a55fff47d7f5181f1f755cfd99
Andrew Carras1 <ajcarras@me.com>
2017-08-02 00:36:55 -0400

    View Controllers set up

---
+++ Collection App/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  Collection App
+//
+//  Created by Andrew Carras1 on 8/2/17.
+//  Copyright © 2017 Andrew Carras1. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var gameImageView: UIImageView!
+
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func photosPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func cameraPressed(_ sender: Any) {
+        
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
